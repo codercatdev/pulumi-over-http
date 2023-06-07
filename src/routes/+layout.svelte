b9f4bfca0e8c70b19c090cd83e4a099769b8107c
Alex <alex@codingcat.dev>
2023-06-07 17:04:20 -0400

    add blackcatui

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script>
+	// Your selected BlackCatUI theme:
+	import '@codingcatdev/blackcatui/dist/themes/theme-blackcatui.css';
+
+	// This contains the bulk of BlackCatUI required styles:
+	import '@codingcatdev/blackcatui/dist/styles/all.css';
+
+	// Finally, your application's global stylesheet (sometimes labeled 'app.css')
+	import '../app.postcss';
+</script>
+
+<slot />
---
+++ tailwind.config.cjs
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	darkMode: 'class',
+	content: [
+		'./src/**/*.{html,js,svelte,ts}',
+		require('path').join(require.resolve('@codingcatdev/blackcatui'), '../**/*.{html,js,svelte,ts}')
+	],
+	theme: {
+		extend: {}
+	},
+	plugins: [...require('@codingcatdev/blackcatui/dist/tailwind/blackcatui.cjs')()]
+};
